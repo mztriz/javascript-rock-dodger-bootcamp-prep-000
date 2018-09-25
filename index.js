75d27b158c9fc45c3a475ebbcf6ead1aad3714f0
Ava Barron <mztriz@gmail.com>
2018-09-25 20:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,18 +75,15 @@ function endGame() {
 }
 
 function moveDodger(e) {
- if ()
- 
- const code = e.which
 
-  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) > -1) {
     e.preventDefault()
     e.stopPropagation()
   }
 
-  if (code === LEFT_ARROW) {
+  if (e.which === LEFT_ARROW) {
     moveDodgerLeft()
-  } else if (code === RIGHT_ARROW) {
+  } else if (e.which === RIGHT_ARROW) {
     moveDodgerRight()
   }
 }
