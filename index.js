3b45fea5cf4c9d8c9c66686fe83313cae23d8afe
Ava Barron <mztriz@gmail.com>
2018-09-25 20:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function moveDodger(e) {
     e.preventDefault()
     e.stopPropagation()
   }
-  // Seriously, what?
+  // Seriously, what is this?? I just saw it was testing for it
   
   
   if (e.which === LEFT_ARROW) {
